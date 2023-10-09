b8f5b32e498bfcbc5279b02cd67775eacc9d3dd0
felipesanderp <felipe.panisson@hotmail.com>
2023-10-09 16:14:22 -0300

    fetch user check ins history paginated

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,4 +4,5 @@ export interface CheckInsRepository {
   // findById(id: string): Promise<CheckIn | null>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
 }
