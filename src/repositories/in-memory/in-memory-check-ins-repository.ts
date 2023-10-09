79d84ae509553da6b7e5d1e8f64dd777fbcc811b
felipesanderp <felipe.panisson@hotmail.com>
2023-10-09 16:59:50 -0300

    get user metrics from check ins use case and tests

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -54,4 +54,8 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       .filter((item) => item.user_id === userId)
       .slice((page - 1) * 20, page * 20)
   }
+
+  async countByUserId(userId: string) {
+    return this.checkIns.filter((item) => item.user_id === userId).length
+  }
 }
