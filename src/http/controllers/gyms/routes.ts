38f84647507454f0673c837ec49171ce5ddaf634
felipesanderp <felipe.panisson@hotmail.com>
2023-10-13 13:57:54 -0300

    gyms routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,6 +2,15 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
+import { search } from './search.controller'
+import { fetchNearby } from './nearby'
+import { create } from './create.controller'
+
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', fetchNearby)
+
+  app.post('/gyms', create)
 }
