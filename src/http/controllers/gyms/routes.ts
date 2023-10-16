fd23c7af188a15e28960ebc7a2e494829ad5704b
felipesanderp <felipe.panisson@hotmail.com>
2023-10-16 10:57:25 -0300

    RBAC middleware

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 import { search } from './search.controller'
 import { fetchNearby } from './nearby.controller'
@@ -12,5 +13,5 @@ export async function gymsRoutes(app: FastifyInstance) {
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', fetchNearby)
 
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
